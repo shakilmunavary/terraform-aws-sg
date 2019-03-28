a2bcc12cc2b47df1773b534247f2f6d98a122d3f
shakilmunavary <shakil.ahamed@gmail.com>
2019-03-28 13:34:42 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,47 @@
+resource "aws_security_group" "main" {
+  name        = "${var.sgname}"
+  description = "security group"
+  vpc_id      = "${var.vpc_id}"
+
+  ingress {
+    protocol  = -1
+    from_port = 0
+    to_port   = 0
+    self      = true
+  }
+
+  ingress {
+    protocol    = "tcp"
+    from_port   = 22
+    to_port     = 22
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    protocol    = "tcp"
+    from_port   = 80
+    to_port     = 80
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    protocol    = "tcp"
+    from_port   = 443
+    to_port     = 443
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    protocol    = "tcp"
+    from_port   = 8800
+    to_port     = 8800
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    protocol    = -1
+    from_port   = 0
+    to_port     = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
