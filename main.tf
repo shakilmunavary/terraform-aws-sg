e2fd3efc82b9ebf3bae2070b3e1548cdf535c6c1
shakilmunavary <shakil.ahamed@gmail.com>
2019-03-28 13:39:14 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
-resource "aws_security_group" "main" {
-  name        = "${var.sgname}"
+resource "aws_security_group" "this" {
+  name        = "${var.name}"
   description = "security group"
   vpc_id      = "${var.vpc_id}"
 
